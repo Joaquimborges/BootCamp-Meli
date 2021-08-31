a71dbbea817da0f51477abb8739f00e47321bedf
joaquimborges <joaquim.borges1993@gmail.com>
2021-08-31 11:43:15 -0300

    Modelagem sistema universidade

---
+++ src/com/company/exercicios/universidade/AlunosTecnicos.java
@@ -0,0 +1,5 @@
+package com.company.exercicios.universidade;
+
+public class AlunosTecnicos extends Pessoa implements SuporteTecnico{
+
+}
---
+++ src/com/company/exercicios/universidade/Tutores.java
@@ -0,0 +1,6 @@
+package com.company.exercicios.universidade;
+
+public class Tutores extends Pessoa implements Professor {
+
+
+}
